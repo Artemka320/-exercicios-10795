e869bf69b4e2b08af769e542c7ad14a3ec862483
Artemka <artemkarolasbugado@gmail.com>
2026-04-17 16:21:53 +0100

    ex8.py

---
+++ loops/ex8.py
@@ -0,0 +1,8 @@
+print("Série 1 (10 em 10):")
+for i in range(10, 1001, 10):
+    print(i, end=" ")
+
+print("\n\nSérie 2 (15, 25, 35...):")
+for i in range(15, 1000, 10):
+    print(i, end=" ")
+print()
